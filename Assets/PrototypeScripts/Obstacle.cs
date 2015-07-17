4b09cbf84a6ec2a76e9cd23af501d922c6de082c
Arindam Bose <arindam.b@gatech.edu>
2015-07-17 13:10:06 -0400

    Obstacles

---
+++ Assets/PrototypeScripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	public float MoveSpeed;
+
+	private Vector2 _moveVector;
+	void Awake(){
+		_moveVector = new Vector2(-1,0);
+	}
+
+	void Start(){
+		UnityTimer.Instance.CallAfterDelay(()=>{
+			Destroy(this.gameObject);
+		}, 5.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position+=(Vector3)_moveVector*MoveSpeed*Time.deltaTime;
+	}
+}
--- Assets/PrototypeScripts/ObstacleSpawner.cs
+++ Assets/PrototypeScripts/ObstacleSpawner.cs
@@ -5,10 +5,30 @@ public class ObstacleSpawner : MonoBehaviour {
 
 	public static ObstacleSpawner Instance;
 
+	public GameObject obstacle;
+	public float spawnInterval;
+
+	private Vector3 _groundPosition;
+	private Vector3 _aerialPosition;
+
 	void Awake(){
 		Instance = this;
 	}
 
+	void Start(){
+		_groundPosition = transform.FindChild("GroundObstacleSpawnPoint").position;
+		_aerialPosition = transform.FindChild("AerialObstacleSpawnPoint").position;
+
+		UnityTimer.Instance.CallRepeating(() =>{
+			int determiner = Random.Range(0, 100);
+			if(determiner < 50){
+				GameObject _obstacle = Instantiate(obstacle, _groundPosition, Quaternion.identity) as GameObject;
+			}
+			else{
+				GameObject _obstacle = Instantiate(obstacle, _aerialPosition, Quaternion.identity) as GameObject;
+			}
+		}, spawnInterval);
+	}
 
 
 
