9086a9136c92d97092dbcf242b26c148cc97a363
kkiniaes <kkiniaes@gmail.com>
2015-07-18 16:12:08 -0400

    updated main menu

---
+++ Assets/LoopingRoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopingRoad : MonoBehaviour {
+
+	public float reposition = 34;
+	public float length = 27;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x > -length) {
+			transform.Translate(new Vector3(-3*Time.deltaTime, 0f, 0f));
+		} else {
+			transform.position = new Vector3(reposition,transform.position.y, transform.position.z);
+		}
+	}
+}
