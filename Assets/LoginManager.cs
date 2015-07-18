2f644c169e7ae7b158c73f4616835483760d5f9a
Deep Ghosh <deep@th.ru.st>
2015-07-18 00:30:25 -0400

    Checkin initial

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameJolt.UI.Manager.Instance.ShowSignIn((bool success) =>
+        {
+            if (success)
+            {
+                Debug.Log("Yaaaa I can code!!!");
+            }
+            else
+            {
+                Debug.Log("Yaaa You can not code!!!");
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    public static void Logout()
+    {
+        var isSignedIn = GameJolt.API.Manager.Instance.CurrentUser != null;
+        if (isSignedIn)
+        {
+            GameJolt.API.Manager.Instance.CurrentUser.SignOut();
+        }
+    }  
+}
