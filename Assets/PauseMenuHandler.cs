e4ce79299a5b18a1e13d27b0d2cf5cc4e97d7708
kkiniaes <kkiniaes@gmail.com>
2015-07-19 04:18:55 -0400

    fixed pause menu

---
+++ Assets/PauseMenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ContinuePressed() {
+		GameStateManager.Instance.State = GameManager.GameState.Running;
+	}
+
+	public void QuitToMenuPressed() {
+		GameStateManager.Instance.State = GameManager.GameState.MainMenu;
+	}
+}
